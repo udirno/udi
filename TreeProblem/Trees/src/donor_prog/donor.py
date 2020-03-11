b59af71bf7d567f30da6bc31236f7a36ed2ab9b8
Udirno Chaudhuri <udirno@gmail.com>
2020-03-11 15:56:25 -0700

    donor progress

--- TreeProblem/Trees/src/donor_prog/donor.py
+++ TreeProblem/Trees/src/donor_prog/donor.py
@@ -2,3 +2,11 @@ class Donor(object):
     """
     Maybe it would be a good idea to a make a simple donor class
     """
+    def __init__(self, name : int, amount : int) -> None:
+        self.name = name
+        self.amount = amount
+
+    def __str__(self) -> str:
+        print(f'{self.name}  with a donation of {self.amount}')
+
+
--- TreeProblem/donor_runner.py
+++ TreeProblem/donor_runner.py
@@ -1,1 +1,27 @@
 # Your solution for the donor problem here
+import sys
+from Trees.src.trees.bst_tree import BST, MissingValueError
+from Trees.src.nodes.bst_node import BSTNode
+from Trees.src.donor_prog.donor import Donor
+
+if __name__ == '__main__':
+    donor_file_path = sys.argv[1]
+    command = sys.argv[2]
+    donor_tree = BST(None, lambda x: x.amount)
+
+    with open(donor_file_path) as cfg:
+        ship_args = dict()
+        for line_num, line in enumerate(cfg):
+            split_line = line.split(" : ")
+            if line_num == 0:
+                donor_name, donor_amount = (int(x.strip()) for x in split_line)
+                #create donor object and add it to the tree
+                donor_info = Donor(donor_name, donor_amount)
+                donor_tree.add_value(donor_info)
+            else:
+                ship_args[split_line[0]] = int(split_line[1])
+    random.seed(int(sys.argv[2]))
+    #print(f'num_rows = {num_rows}, num_cols = {num_cols}')
+    #print(f'ships = {ship_args}')
+    game = Game(ship_args, num_rows, num_cols)
+    game.play()
